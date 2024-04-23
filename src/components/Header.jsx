1f0137a424e68abc312a7fc502efb077e725af80
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-04-23 21:01:26 +0600

    signin and signup page setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,15 +6,18 @@ import {Plus} from 'lucide-react'
 import { useEffect } from 'react'
 import {usePathname} from 'next/navigation'
 import Link from 'next/link'
+import { UserButton } from '@clerk/nextjs'
+import { useUser } from '@clerk/nextjs'
 const Header = () => {
     const path = usePathname();
+    const {user, isSignedIn}= useUser();
     useEffect(()=>{
         
     },[])
   return (
     <div className='w-5/6 m-auto flex justify-between py-5'>
         <div className='flex  items-center gap-5'>
-            <Link href={'/'}><Image src={'/images/logo.svg'} width={150} height={150}></Image></Link>
+            <Link href={'/'}><Image src={'/images/logo.svg'} width={150} height={150} alt='logo'></Image></Link>
             <ul className='flex gap-5'>
                 <Link href={'/ForSell'} className='hover:text-red-500 hover:font-bold'>For sell</Link>
                 <Link href={'/Rent'} className='hover:text-red-500 hover:font-bold'>For rent</Link>
@@ -23,7 +26,8 @@ const Header = () => {
         </div>
         <div className='flex gap-2'>
             <Button className='flex gap-2' > <Plus/> Add your post</Button>
-            <Button variant='outline' >Login</Button>
+            {isSignedIn ? <UserButton/>: <Button variant='outline' >Login</Button>}
+            
         </div>
     </div>
   )
